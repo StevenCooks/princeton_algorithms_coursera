f8709bc5971e6f96b6cf50eb0f60a7386513e5bc
Steven Cooks <stevenjoy1113@gmail.com>
2015-07-23 20:23:32 -0400

    add assignment2 test skeleton

---
+++ Assignments/A02_RandomizedQueuesAndDeques/test/DequeTest.java
@@ -0,0 +1,79 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * 
+ */
+
+/**
+ * @author Steven Cooks
+ *
+ */
+public class DequeTest {
+
+    /**
+     * Test method for {@link Deque#Deque()}.
+     */
+    @Test
+    public void testDeque() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link Deque#isEmpty()}.
+     */
+    @Test
+    public void testIsEmpty() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link Deque#size()}.
+     */
+    @Test
+    public void testSize() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link Deque#addFirst(java.lang.Object)}.
+     */
+    @Test
+    public void testAddFirst() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link Deque#addLast(java.lang.Object)}.
+     */
+    @Test
+    public void testAddLast() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link Deque#removeFirst()}.
+     */
+    @Test
+    public void testRemoveFirst() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link Deque#removeLast()}.
+     */
+    @Test
+    public void testRemoveLast() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link Deque#iterator()}.
+     */
+    @Test
+    public void testIterator() {
+        fail("Not yet implemented"); // TODO
+    }
+
+}
---
+++ Assignments/A02_RandomizedQueuesAndDeques/test/RandomizedQueueTest.java
@@ -0,0 +1,48 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class RandomizedQueueTest {
+
+    @Test
+    public void testRandomizedQueue() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public void testIsEmpty() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public void testSize() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public void testEnqueue() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public void testDequeue() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public void testSample() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public void testIterator() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public void testMain() {
+        fail("Not yet implemented"); // TODO
+    }
+
+}
