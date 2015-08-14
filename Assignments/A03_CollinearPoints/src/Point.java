cbfbed25d9c4de1f3bf4b7f1179e3c320fed820f
Steven Cooks <stevenjoy1113@gmail.com>
2015-08-13 20:31:38 -0400

    update Point class

--- Assignments/A03_CollinearPoints/src/Point.java
+++ Assignments/A03_CollinearPoints/src/Point.java
@@ -7,9 +7,8 @@ public class Point implements Comparable<Point> {
 
     public final Comparator<Point> SLOPE_ORDER = new Comparator<Point>() {
         @Override
-        public int compare(Point o1, Point o2) {
-            // TODO Auto-generated method stub
-            return 0;
+        public int compare(Point p1, Point p2) {
+            return (int) (slopeTo(p1) - slopeTo(p2));
         }
     };
 
@@ -24,11 +23,13 @@ public class Point implements Comparable<Point> {
         StdDraw.point(x, y);
     }
 
+    // plot this point to standard drawing
     public void drawTo(Point that) {
         /* DO NOT MODIFY */
         StdDraw.line(this.x, this.y, that.x, that.y);
     }
 
+    // draw line between this point and that point to standard drawing
     @Override
     public String toString() {
         /* DO NOT MODIFY */
@@ -37,12 +38,42 @@ public class Point implements Comparable<Point> {
 
     @Override
     public int compareTo(Point that) {
-        // TODO Auto-generated method stub
-        return 0;
+        if (this.x == that.x && this.y == that.y) {
+            return 0;
+        } else if (this.y < that.y || (this.y == that.y && this.x < that.x)) {
+            return -1;
+        } else {
+            return 1;
+        }
     }
 
+    /**
+     * Returns slope between this point and that point.
+     * <p>
+     * Return positive infinity for vertical line segment; return negative infinity
+     * for a line between a point and itself; otherwise, (that.y - this.y) /
+     * (that.x - this.x).
+     * 
+     * @param that argument point
+     * @return slope between invoking point and argument point
+     */
     public double slopeTo(Point that) {
-        return 0.0;
+        if (this.x == that.x) {
+            if (this.y == that.y) {
+                // a line between a point and itself
+                return Double.NEGATIVE_INFINITY;
+            } else {
+                // vertical line segment
+                return Double.POSITIVE_INFINITY;
+            }
+        } else {
+            if (this.y == that.y) {
+                // horizontal line => return positive 0
+                return 0;
+            } else {
+                return 1.0 * (that.y - this.y) / (that.x - this.x);
+            }
+        }
     }
 
 }
