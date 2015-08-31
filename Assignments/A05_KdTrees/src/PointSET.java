625c4e4249f01fa728af53341c0fbe6eeed10919
Steven Cooks <stevenjoy1113@gmail.com>
2015-08-30 22:50:57 -0400

    add assignment 5 skeleton

---
+++ Assignments/A05_KdTrees/src/PointSET.java
@@ -0,0 +1,43 @@
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+
+/**
+ * 
+ */
+public class PointSET {
+    
+    public PointSET() {
+    }
+
+    public int size() {
+        return 0;
+    }
+    
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public void insert(Point2D p) {
+        
+    }
+    
+    public boolean contains(Point2D p) {
+        return false;
+    }
+    
+    public void draw() {
+    }
+    
+    public Iterable<Point2D> range(RectHV rect) {
+        return null;
+    }
+    
+    public Point2D nearest(Point2D p) {
+        return null;
+    }
+    
+    public static void main(String[] args) {
+
+    }
+
+}
