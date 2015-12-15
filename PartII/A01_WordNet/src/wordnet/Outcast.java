4e5f7d7fb539c7d5593ebebd35e618409ed8d1d4
Steven Cooks <stevenjoy1113@gmail.com>
2015-12-15 17:20:50 -0500

    code skeleton

---
+++ PartII/A01_WordNet/src/wordnet/Outcast.java
@@ -0,0 +1,19 @@
+package wordnet;
+
+public class Outcast {
+    
+    // constructor takes a WordNet object
+    public Outcast(WordNet wordnet) { }
+
+    // given an array of WordNet nouns, return an outcast
+    public String outcast(String[] nouns) {
+        return "";
+    }
+    
+    // set test client below
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
